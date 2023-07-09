1b89aacbbe3b1756c052ba7c66f09ac77a099905
Ashish <a.kumar@rowthtech.com>
2023-07-09 15:56:52 +0530

    new project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { CartComponent } from './cart/cart.component';
+import { CartModule } from './cart/cart.module';
+import { HeaderComponent } from './theme/header/header.component';
+import { FooterComponent } from './theme/footer/footer.component';
+import { SidebarComponent } from './theme/sidebar/sidebar.component';
+import { MultiSelectDropdownComponent } from './shared/multi-select-dropdown/multi-select-dropdown.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CheckoutModule } from './checkout/checkout.module';
+import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    MultiSelectDropdownComponent,
+    CheckoutComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    CartModule,
+    CheckoutModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/multi-select-dropdown/multi-select-dropdown.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-select-dropdown',
+  templateUrl: './multi-select-dropdown.component.html',
+  styleUrls: ['./multi-select-dropdown.component.scss']
+})
+export class MultiSelectDropdownComponent {
+
+}
